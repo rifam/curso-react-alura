236cea70ac941906291540a5ce4e9b508d660c67
rifam <vinicius.rifam@gmail.com>
2020-03-12 17:02:02 -0300

    Dale :tada: :tada: :tada:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import "./App.css"
+import Header from "./Header";
+import Tabela from "./Tabela";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from "./Formulario";
+
+class App extends Component {
+  state = {
+    autores: [
+      {
+        nome: "Paulo",
+        livro: "React",
+        preco: "1000"
+      },
+      {
+        nome: "Daniel",
+        livro: "Java",
+        preco: "99"
+      },
+      {
+        nome: "Marcos",
+        livro: "Design",
+        preco: "150"
+      },
+      {
+        nome: "Bruno",
+        livro: "DevOps",
+        preco: "100"
+      },
+      {
+        nome: "Nico",
+        livro: "JavaScript",
+        preco: "111"
+      }
+    ]
+  };
+
+  removeAutor = index => {
+    const { autores } = this.state;
+    this.setState({
+      autores: autores.filter((autor, posAtual) => {
+        return posAtual !== index;
+      })
+    });
+  };
+
+  escutadorDeSubmit = autor => {
+    this.setState({ autores: [...this.state.autores, autor] });
+  };
+
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="container mb-10">
+          <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+          <Form escutadorDeSubmit={this.escutadorDeSubmit} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/FormValidator.js
@@ -0,0 +1,12 @@
+import validator from "validator"
+
+
+class FormValidator{
+    valida(state){
+        console.log('validado');
+        return false;
+    }
+
+}
+
+export default FormValidator;
